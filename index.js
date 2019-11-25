ab437cea7ec12c99b64c5e542ddf0a92da093dcd
sesamestrong <doolie.healey@gmail.com>
2019-11-25 01:25:46 -0500

    Adding SSL

--- index.js
+++ index.js
@@ -21,6 +21,13 @@ const shareWithEmail = ({
         projectName,
         template,
     });
+// Read the Certbot response from an environment variable; we'll set this later:
+const letsEncryptReponse = process.env.CERTBOT_RESPONSE;
+// Return the Let's Encrypt certbot response:
+app.get('/.well-known/acme-challenge/:content', function(req, res) {
+  res.send(letsEncryptReponse);
+});
+
 app.get("/", (req, res) =>res.send("API call is /:projectName/:email?template=none"));
 app.get("/create/:projectName/:email", async (req, res) => {
     const {
