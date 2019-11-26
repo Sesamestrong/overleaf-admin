a6a1df440ab5a1855dcd0ed3d489aa8a7048e477
Sesamestrong <doolie.healey@gmail.com>
2019-11-26 04:04:27 +0000

    Add new features, docs

--- index.js
+++ index.js
@@ -6,10 +6,11 @@ const {
 } = require("@sesamestrong/json-scraper");
 
 const overleaf=require("./overleaf.json");
+const share=require("./share.json");
 //console.log(overleaf.steps.length);
 
 //})().catch(err=>console.log(err.toString(),err.jsonData,err.stepNumber));
-const shareWithEmail = ({
+const createAndShare = ({
         invitee,
   projectName,
   template="none"
@@ -21,7 +22,8 @@ const shareWithEmail = ({
         projectName,
         template,
     });
-app.get("/", (req, res) =>res.send("API call is /:projectName/:email?template=none"));
+const shareWithEmail=({invitee,projectId})=>runEntireScraper(share,{email:invitee,project_id:projectId});
+app.get("/", (req, res) =>res.sendFile(__dirname+"/index.html"));
 app.get("/create/:projectName/:email", async (req, res) => {
     const {
         projectName,
@@ -33,7 +35,7 @@ app.get("/create/:projectName/:email", async (req, res) => {
     if (projectName && email) {
       if(email.match(process.env.ALLOWED_EMAILS)==null) return res.send({err:"Disallowed email."});
       try{
-        const {expires}=await shareWithEmail({projectName,invitee:email,template});
+        const {expires}=await createAndShare({projectName,invitee:email,template});
         return res.send({expires});
       }
       catch(err){
@@ -48,7 +50,29 @@ app.get("/create/:projectName/:email", async (req, res) => {
       }
     }
     return res.send({
-        err: "Must include project name and email"
+        err: "Must include projectName and email"
     });
+});
+
+app.get("/share/:projectId/:email",async (req,res)=>{
+    const {projectId,email}=req.params;
+    if(projectId&&email){
+        if(email.match(process.env.ALLOWED_EMAILS)==null) return res.send({err:"Disallowed email."});
+        try{
+            const {expires}=await shareWithEmail({projectId,invitee:email});
+            return res.send({expires});
+        }
+        catch(err){
+            res.send({err:"I'm not telling what the error is, but rest assured that it's been logged."});
+            console.log("SHARE ERROR:");
+            if(err.jsonData){
+            const {invitee,template,csrf,expires}=err.jsonData;
+            console.log("Data:",invitee,template,csrf,expires);
+            }
+            console.log("Step index:",err.stepNumber);
+            throw err;
+        }
+    }
+    return res.send({err:"Must include projectId and email"});
 });
 app.listen(process.env.PORT||3000);
